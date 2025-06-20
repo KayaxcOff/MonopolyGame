4654fce6dcb8eb4cfa2398b7d4423a1fa19f6192
Muhammet Kaya <muham123cak@gmail.com>
2025-06-20 18:18:28 +0300

    monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -1,47 +1,52 @@
 import json
 import random
 
+file_path = "monoGame.json"
+
 class MonopolyGame:
-    @staticmethod
-    def read_json(file_name):
-        with open(file_name, 'r') as f:
-            monopoly_data = json.load(f)
-        return monopoly_data
+    def __init__(self):
+        pass
 
-    def __init__(self, player_data):
-        self.field = [None] * 40
-        self.player = Player(player_data)
+    def players(self):
+        with open(file_path) as json_file:
+            self.players_data = json.load(json_file)
+        self.name = self.players_data[0]["name"]
+        self.cash = self.players_data[0]["cash"]
+        self.debt = self.players_data[0]["debt"]
+        self.properties = self.players_data[0]["properties"]
+        self.position = self.players_data[0]["position"]
 
-    @staticmethod
-    def roll_dice():
-        die_1 = random.randint(1, 6)
-        die_2 = random.randint(1, 6)
-        return die_1 + die_2
+    def roll_dice(self):
+        self.dice_1 = random.randint(1, 6)
+        self.dice_2 = random.randint(1, 6)
+        self.total_roll = self.dice_1 + self.dice_2
+        return self.total_roll
+
+    def gameField(self):
+        self.matris = [None] * 40
+        self.matris[random.randint(0, 39)] = {"type" : "house", "name" : "UltraTech"}
+        self.matris[random.randint(0, 39)] = {"type" : "house", "name" : "Bank"}
+        self.matris[random.randint(0, 39)] = {"type" : "house", "name" : "Google House"}
+        self.matris[random.randint(0, 39)] = {"type" : "house", "name" : "Prison"}
+        self.matris[random.randint(0, 39)] = {"type" : "house", "name" : "Home of the Future"}
+        return self.matris
+
+class MonopolyGamePlayer:
+    def __init__(self):
+        pass
+
+    def player_info(self):
+        self.players()
+
+    def player_position(self):
+        pass
 
     def pay_rent(self):
-        self.rent_light = 10
-        self.rent_normal = 25
-        self.rent_heavy = 50
-
-class Player:
-    def __init__(self, data):
-        self.name = data['name']
-        self.money = data['cash']
-        self.debt = data['debt']
-        self.position = data['position']
-
-    def move(self, steps, board_size=40):
-        self.position = (self.position + steps) % board_size
-
-    def properties(self):
-        self.mediterranean_avenue
-        self.boardwalk
-
-data = MonopolyGame.read_json('monoGame.json')
-player_data = data['players'][0]
-game = MonopolyGame(player_data)
-
-dice = MonopolyGame.roll_dice()
-game.player.move(dice)
-
-print(f"{game.player.name} new position: {game.player.position}")
+        self.mono = MonopolyGame()
+        self.rentForProperty = {
+            "UltraTech": 100,
+            "Bank": 150,
+            "Google House": 200,
+            "Prison": 250,
+            "Home of the Future": 300
+        }
